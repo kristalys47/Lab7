291f07c50f8fb2886dfb2527a649e4658b84f459
kristalys47 <kristalys.cba4@yahoo.com>
2017-06-09 01:47:55 -0400

    ej 5-8

--- lab7/src/Sign.java
+++ lab7/src/Sign.java
@@ -1,23 +1,29 @@
-public class Sign implements GameThing, Readable
+public class Sign implements GameThing, Readable, Edible
 
 {
 
       public String getName()
 
       {
-            return "A wooden sign";
+            return "A chocolate wooden sign";
       }
 
       public String getDescription()
 
       {
-                return "This is a small wooden sign. There seems to be some partially faded writing in it.";
+                return "This is a small chocolate wooden sign. There seems to be some partially faded \nwriting in it, but forget it; IT IS CHOCOLATE! Try eating it.";
       }
 
       public void read()
 
       {
-            System.out.println("You can see the following message: \"BLUP!\"");
+            System.out.println("You can read the following message: \"Eat me :)!\"");
       }
+
+	@Override
+	public void eat() {
+		System.out.println("This is better than Godiva's Chocolate. Very creamy! I think it has almonds.");
+		AdventureEpsilon.theRoom.removeThing(AdventureEpsilon.theSign);
+	}
 
 }
